6967bb3ee42e55a02d095278e3ea1dee8f27e27c
glowlh <m.o.gushchina@gmail.com>
2025-04-29 00:25:02 +0300

    feat add multiple Windows Observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box } from "./styles.ts";
+import { WindowObserver } from './WindowObserver';
+const wo = new WindowObserver();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleOpenNewWindow = (event) => {
+    window.open('/', `window ${self.crypto.randomUUID()}`, 'width=600,height=400');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box>
+      Window {wo.id}
+      <button onClick={() => wo.stop()}>Stop</button>
+      <button onClick={() => wo.clear()}>Clear</button>
+      <button onClick={handleOpenNewWindow}>Open new window</button>
+    </Box>
+  );
 }
 
 export default App
